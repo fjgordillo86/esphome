6279211a48570ac62b3d81716fa8dc666166b53f
fjgordillo86 <59791834+fjgordillo86@users.noreply.github.com>
2025-08-03 19:04:02 +0200

    Update climate.py

--- esphome/components/madoka/climate.py
+++ esphome/components/madoka/climate.py
@@ -14,11 +14,7 @@ Madoka = madoka_ns.class_(
 )
 
 CONFIG_SCHEMA = (
-    climate.CLIMATE_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(Madoka),
-        }
-    )
+    climate.climate_schema(Madoka)
     .extend(ble_client.BLE_CLIENT_SCHEMA)
     .extend(cv.polling_component_schema("10s"))
 )
